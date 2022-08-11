2e3035d7e39d3a372789e373a9bbc20fb4852488
khumayunkh <hasanovhumayun15@gmail.com>
2022-08-11 19:47:02 +0500

    body

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -25,6 +25,46 @@ function Body(){
                         <h3>dwdaddcrrrrrrrr</h3>
                         <h3>dwdaddcrrrrrrrr</h3>
                    </div>
+                   <div className={style.country}>
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Flag_of_Uzbekistan.svg/1280px-Flag_of_Uzbekistan.svg.png"/>
+                        <h2>Flag_of_Uzbekistan</h2>
+                        <h3>dwdaddcrrrrrrrr</h3>
+                        <h3>dwdaddcrrrrrrrr</h3>
+                        <h3>dwdaddcrrrrrrrr</h3>
+                        <h3>dwdaddcrrrrrrrr</h3>
+                   </div>
+                   <div className={style.country}>
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Flag_of_Uzbekistan.svg/1280px-Flag_of_Uzbekistan.svg.png"/>
+                        <h2>Flag_of_Uzbekistan</h2>
+                        <h3>dwdaddcrrrrrrrr</h3>
+                        <h3>dwdaddcrrrrrrrr</h3>
+                        <h3>dwdaddcrrrrrrrr</h3>
+                        <h3>dwdaddcrrrrrrrr</h3>
+                   </div>
+                   <div className={style.country}>
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Flag_of_Uzbekistan.svg/1280px-Flag_of_Uzbekistan.svg.png"/>
+                        <h2>Flag_of_Uzbekistan</h2>
+                        <h3>dwdaddcrrrrrrrr</h3>
+                        <h3>dwdaddcrrrrrrrr</h3>
+                        <h3>dwdaddcrrrrrrrr</h3>
+                        <h3>dwdaddcrrrrrrrr</h3>
+                   </div>
+                   <div className={style.country}>
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Flag_of_Uzbekistan.svg/1280px-Flag_of_Uzbekistan.svg.png"/>
+                        <h2>Flag_of_Uzbekistan</h2>
+                        <h3>dwdaddcrrrrrrrr</h3>
+                        <h3>dwdaddcrrrrrrrr</h3>
+                        <h3>dwdaddcrrrrrrrr</h3>
+                        <h3>dwdaddcrrrrrrrr</h3>
+                   </div>
+                   <div className={style.country}>
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Flag_of_Uzbekistan.svg/1280px-Flag_of_Uzbekistan.svg.png"/>
+                        <h2>Flag_of_Uzbekistan</h2>
+                        <h3>dwdaddcrrrrrrrr</h3>
+                        <h3>dwdaddcrrrrrrrr</h3>
+                        <h3>dwdaddcrrrrrrrr</h3>
+                        <h3>dwdaddcrrrrrrrr</h3>
+                   </div>
                 </div>
             </div>
         </div>

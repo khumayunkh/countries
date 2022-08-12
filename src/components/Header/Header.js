367e79c768ab0935155408f81790aa0c4d584d17
khumayunkh <hasanovhumayun15@gmail.com>
2022-08-12 17:20:16 +0500

    input

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,14 +1,23 @@
-import React from "react";
+import React, { useRef } from "react";
+import { useDispatch } from "react-redux";
+import { countriesAction } from "../../store/CountriesReducer";
 import style from './Header.module.css'
 
 
 function Header(){
+    const inputRef = useRef('')
+    const dispatch = useDispatch()
+
+    const filterByName = () => {
+        dispatch(countriesAction.searchCountry(inputRef.current.value))
+    }
+
     return(
         <>
         <div className={style.header}>
             <div className={style.container}>
                 <div className={style.header_in}>
-                    <input placeholder="Search..." className={style.input}/>
+                    <input ref={inputRef} onChange={filterByName} placeholder="Search..." className={style.input}/>
                 </div>
             </div>
         </div>

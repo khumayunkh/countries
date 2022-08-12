2dd392ea3ff040bb96032b04e9aa43f97de98656
khumayunkh <hasanovhumayun15@gmail.com>
2022-08-12 16:24:42 +0500

    pagination

--- src/components/Body/Body.js
+++ src/components/Body/Body.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { getCountriesThunk } from "../../store/CountriesReducer";
@@ -6,15 +6,23 @@ import style from './Body.module.css'
 
 function Body(){
     const {countries} = useSelector(state => state.countries)
-    const dispatch = useDispatch()
+    const dispatch = useDispatch() 
+    const [currentPage, setCurrentPage] = useState(1)
+    const [countriesPerPage, serCountriesPerPage] = useState(9)
     
     useEffect(() => {
         dispatch(getCountriesThunk())
-    },[])
+    },[]) 
+
+    const indexOfLastPost = currentPage * countriesPerPage;
+    const indexOfFirstPost = indexOfLastPost - countriesPerPage;
+    const currentPosts = countries.slice(indexOfFirstPost, indexOfLastPost)
     
+    const paginate = pageNumber => setCurrentPage(pageNumber)
+
     return(
-        <>
-        <div className={style.countries}>
+        <> 
+         <div className={style.countries}>
             <div className={style.container}>
                 <div className={style.countries_in}>
                    <div className={style.country}>
